bf4974fdd3bb1501f73cecdcef87084d0c1366b9
Daihecyy <paul.saubusse@gmail.com>
2024-05-22 11:17:32 +0200

    Working prototype of card_product_variant

--- src/components/user/card-product-variant.tsx
+++ src/components/user/card-product-variant.tsx
@@ -9,6 +9,13 @@ import {
   CardHeader,
   CardTitle,
 } from "@/components/ui/card"
+import { Button } from "@/components/ui/button"
+import {
+  Accordion,
+  AccordionContent,
+  AccordionItem,
+  AccordionTrigger,
+} from "@/components/ui/accordion"
 
 type ProductVariant = {
   price: number
@@ -17,42 +24,50 @@ type ProductVariant = {
 
 
 export default function CardProductVariant() {
-  const productDescription = "Permet de pratiquer toutes les activités de l'AE";
+  const productDescription = "Permet de pratiquer toutes les activités de l'AE.";
   const productName = "Adhésion AE";
   const variants: { [id: string]: ProductVariant; } = {
-    "Aucun": { "price": 0 }, "1 an": { "price": 200 },
-    "3 ans": { "price": 400, description: "Prends en compte toute la scolarité, même en cas de césure" }
+    "Non": { "price": 0, description: "Attention, vous ne pourrez pas participer ..." },
+    "1 an": { "price": 200, description: "Uniquement 1 année" },
+    "3 ans": { "price": 400, description: "Prends en compte toute la scolarité, même en cas de césure." }
   };
-  const [toggleValue, setToggleValue] = useState('Aucun');
+  const [toggleValue, setToggleValue] = useState('Non');
   const price = variants[toggleValue].price;
-  const variantDescription = variants[toggleValue].description;
+  const variantDescription = (id: string) => variants[id].description;
+  const cardColor = (id: string) => ((id === toggleValue) ? "zinc-300" : "white");
   return (
     <main className="flex [&>div]:w-full h-screen">
-      <Card className="rounded-xl border bg-card text-card-foreground shadow max-w-[700px] m-auto text-zinc-700">
+      <Card className="rounded-xl border bg-card text-card-foreground shadow min-w-[700px] m-auto text-zinc-700">
         <CardHeader>
-          <CardTitle>
-            <span>{productName}</span>
-          </CardTitle>
+          <CardTitle>Sélection des produits</CardTitle>
           <CardDescription>
-            <span>{price + " €"}</span>
           </CardDescription>
         </CardHeader>
         <CardContent>
-          <span>{productDescription}</span><br />
-          <span>{variantDescription}</span>
+          <Accordion type="single" collapsible>
+            <AccordionItem value="Produit">
+              <AccordionTrigger>{productName + " - " + toggleValue + " : " + price + " €"}</AccordionTrigger>
+              <AccordionContent>
+                <span>{productDescription}</span>
+                <div className="grid grid-cols-3 gap-x-3 items-stretch">
+                  {Object.keys(variants).map((id) => (
+                    <Card key={id} onClick={() => setToggleValue(id)} className={"bg-" + cardColor(id) + " border" + ((id === toggleValue) ? "-[20px]" : "")}>
+                      <CardHeader>
+                        <CardTitle>
+                          {id}
+                        </CardTitle>
+                        <CardDescription>{variants[id].price + "€"}</CardDescription>
+                      </CardHeader>
+                      <CardContent>
+                        {variantDescription(id) !== "" ? <span>{variantDescription(id)}</span> : null}
+                      </CardContent>
+                    </Card>
+                  ))}
+                </div>
+              </AccordionContent>
+            </AccordionItem>
+          </Accordion>
         </CardContent>
-        <CardFooter>
-          <ToggleGroup type="single"
-            value={toggleValue}
-            onValueChange={(value: string) => {
-              if (value) setToggleValue(value);
-            }}
-          >
-            {Object.keys(variants).map((id) => (
-              <ToggleGroupItem value={id} key={id}>{id}</ToggleGroupItem>
-            ))}
-          </ToggleGroup>
-        </CardFooter>
       </Card>
     </main>
   );

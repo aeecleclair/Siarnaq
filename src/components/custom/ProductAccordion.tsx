8c37dc90496d687f397bd4e916be46454c0cacb2
Maxime Roucher <maximeroucher@gmail.com>
2024-05-25 10:54:13 +0200

    feat: moving description to accordion trigger

--- src/components/custom/ProductAccordion.tsx
+++ src/components/custom/ProductAccordion.tsx
@@ -18,12 +18,12 @@ export const ProductAccordion = ({ product }: ProductAccordionProps) => {
   return (
     <AccordionItem value={product.id}>
       <AccordionTrigger>
-        <div className="flex items-center justify-between">
+        <div className="flex flex-col items-start justify-between">
           <h3 className="text-lg font-semibold">{product.name_en}</h3>
+          <p className="text-sm text-gray-500">{product.description_en}</p>
         </div>
       </AccordionTrigger>
       <AccordionContent>
-        <p className="text-sm text-gray-500">{product.description_en}</p>
         {/* Take care to export all grid-cols-n
         Can't find a better way to do it for naw */}
         <div className="hidden grid-cols-5" />
@@ -31,7 +31,7 @@ export const ProductAccordion = ({ product }: ProductAccordionProps) => {
         <div className="hidden grid-cols-3" />
         <div className="hidden grid-cols-2" />
         <div className="hidden grid-cols-1" />
-        <div className={`grid grid-cols-${numberOfCard} gap-4 mt-4`}>
+        <div className={`grid grid-cols-${numberOfCard} gap-4`}>
           {product.variants && (
             <>
               {product.variants.map((variant) => (
@@ -39,7 +39,7 @@ export const ProductAccordion = ({ product }: ProductAccordionProps) => {
                   key={variant.id}
                   variant={variant}
                   // This is a dummy value, it should be managed by the parent component
-                  numberSelected={1}
+                  numberSelected={0}
                 />
               ))}
             </>

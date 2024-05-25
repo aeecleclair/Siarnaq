e35562cac601c72fbda88467c16d31fecae23d1b
Maxime Roucher <maximeroucher@gmail.com>
2024-05-25 22:42:58 +0200

    feat: saving current tab as query param

--- src/components/admin/sellerProducts/SellerTab.tsx
+++ src/components/admin/sellerProducts/SellerTab.tsx
@@ -2,12 +2,14 @@ import { Tabs } from "@/components/ui/tabs";
 import { SellerTabList } from "./SellerTabList";
 import { SellerTabContentList } from "./SellerTabContentList";
 import { sellers } from "./sellers";
+import { useSearchParams } from "next/navigation";
 
 export const SellerTab = () => {
-  const firstSeller = sellers[0];
+  const searchParams = useSearchParams();
+  const firstSellerId = searchParams.get("sellerId") || sellers[0].id;
   return (
     <div className="flex items-center justify-center p-6 min-w-96">
-      <Tabs defaultValue={firstSeller.id} className="w-full">
+      <Tabs defaultValue={firstSellerId} className="w-full">
         <SellerTabList />
         <SellerTabContentList />
       </Tabs>
--- src/components/admin/sellerProducts/SellerTabList.tsx
+++ src/components/admin/sellerProducts/SellerTabList.tsx
@@ -1,11 +1,23 @@
 import { TabsList, TabsTrigger } from "@/components/ui/tabs";
 import { sellers } from "./sellers";
+import { useRouter } from "next/navigation";
 
 export const SellerTabList = () => {
+  const router = useRouter();
+
+  const handleClick = (sellerId: string) => {
+    router.push(`/admin/?sellerId=${sellerId}`);
+  };
+
   return (
     <TabsList className={`"grid w-full grid-cols-${sellers.length}"`}>
       {sellers.map((seller) => (
-        <TabsTrigger key={seller.id} value={seller.id} className="w-full min-w-18">
+        <TabsTrigger
+          key={seller.id}
+          value={seller.id}
+          className="w-full min-w-18"
+          onClick={() => handleClick(seller.id)}
+        >
           {seller.name}
         </TabsTrigger>
       ))}

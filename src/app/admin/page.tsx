c56005dbf075a4e769ead99c61f9af9ad23d2e44
Maxime Roucher <maximeroucher@gmail.com>
2024-05-25 22:49:41 +0200

    fix adding suspense for useSearchParam

--- src/app/admin/page.tsx
+++ src/app/admin/page.tsx
@@ -9,23 +9,26 @@ import {
   ResizablePanel,
   ResizableHandle,
 } from "@/components/ui/resizable";
+import { Suspense } from "react";
 
 const AdminPage = () => {
   const { setSize } = useSizeStore();
 
   return (
     <main className="grid flex-1 items-start gap-4 p-4 sm:px-6 sm:py-0 md:gap-8 mt-8">
-      <Card>
-        <ResizablePanelGroup direction="horizontal">
-          <ResizablePanel defaultSize={50} minSize={10}>
-            <UserSearch />
-          </ResizablePanel>
-          <ResizableHandle withHandle />
-          <ResizablePanel defaultSize={50} minSize={10} onResize={setSize}>
-            <SellerTab />
-          </ResizablePanel>
-        </ResizablePanelGroup>
-      </Card>
+      <Suspense fallback={<div>Loading...</div>}>
+        <Card>
+          <ResizablePanelGroup direction="horizontal">
+            <ResizablePanel defaultSize={50} minSize={10}>
+              <UserSearch />
+            </ResizablePanel>
+            <ResizableHandle withHandle />
+            <ResizablePanel defaultSize={50} minSize={10} onResize={setSize}>
+              <SellerTab />
+            </ResizablePanel>
+          </ResizablePanelGroup>
+        </Card>
+      </Suspense>
     </main>
   );
 };

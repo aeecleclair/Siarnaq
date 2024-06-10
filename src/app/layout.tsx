64f43bf94d9c595cce4e64b44284440d98a4081c
Maxime Lemonnier <m38.lemonnier@gmail.com>
2024-06-10 15:28:18 +0200

    fix build

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,7 @@
 import "./globals.css";
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
+import { Suspense } from "react";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -15,8 +16,11 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en">
-      <body className={inter.className}>{children}</body>
-    </html>
+    <Suspense>
+      <html lang="en">
+        <body className={inter.className}>{children}</body>
+      </html>
+    </Suspense>
+    
   );
 }

f46e8e11e360740a35bac5732c57a884d29dfc49
Maxime Roucher <maximeroucher@gmail.com>
2024-06-13 14:08:15 +0200

    feat: using real user list

--- src/components/admin/userSearch/UserSearch.tsx
+++ src/components/admin/userSearch/UserSearch.tsx
@@ -1,8 +1,29 @@
 import { columns } from "./Columns";
 import { DataTable } from "./DataTable";
-import { users } from "./users";
+import { CoreUserSimple, getUsers } from "@/api";
+import { useEffect, useState } from "react";
+
 
 export const UserSearch = () => {
+  const [users, setUsers] = useState<CoreUserSimple[]>([]);
+  const [refetchUsers, setRefetchUsers] = useState<boolean>(true);
+
+  const onGetCdrSellers = async () => {
+    const { data, error } = await getUsers({});
+    if (error) {
+      console.log(error);
+      return;
+    }
+    setUsers(data!);
+  };
+  
+  useEffect(() => {
+    if (refetchUsers) {
+      onGetCdrSellers();
+      setRefetchUsers(false);
+    }
+  }, [refetchUsers]);
+
   return (
     <div className="flex items-center justify-center p-6 min-w-96">
       <DataTable columns={columns} data={users} />

4edb8af68d64a598347c8a4b0f83d6caa5cbceb0
Maxime Lemonnier <m38.lemonnier@gmail.com>
2024-06-15 22:32:37 +0200

    fix error in provider

--- src/app/provider.tsx
+++ src/app/provider.tsx
@@ -1,17 +1,17 @@
 "use client";
 
 import { useToken } from "@/hooks/useToken";
-import { useTokenStore } from "@/stores/token";
 import { client, createClient } from "@hey-api/client-fetch";
 
 export default function Providers({ children }: { children: React.ReactNode }) {
+  const { getToken } = useToken()
   createClient({
     // set default base url for requests
     baseUrl: process.env.NEXT_PUBLIC_BACKEND_URL || "https://hyperion.myecl.fr",
   });
 
   client.interceptors.request.use(async (request) => {
-    const token = useTokenStore();
+    const token = getToken();
     request.headers.set("Authorization", `Bearer ${token}`);
     return request;
   });

bcfb66cdb1fea6e2e841e3e8326f975b415a855d
Maxime Roucher <maximeroucher@gmail.com>
2024-06-30 23:50:01 +0200

    fix: resolving react query / react intl conflict

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,27 +1,41 @@
-"use client";
-
 import "./globals.css";
 import Provider from "./provider";
-import { QueryClientProvider, QueryClient } from "@tanstack/react-query";
+import { QueryProvider } from "./queryProvider";
 import type { Metadata } from "next";
+import { NextIntlClientProvider } from 'next-intl';
+import { getLocale, getMessages } from "next-intl/server";
 import { Inter } from "next/font/google";
 import { Suspense } from "react";
 
-const inter = Inter({ subsets: ["latin"] });
-const queryClient = new QueryClient();
 
-export default function RootLayout({
+const inter = Inter({ subsets: ["latin"] });
+
+export const metadata: Metadata = {
+  title: "Chaîne de rentrée",
+  description: "Plateforme de gestion de la rentrée associative de Centrale Lyon",
+};
+
+export default async function RootLayout({
   children,
 }: Readonly<{
   children: React.ReactNode;
 }>) {
+  const locale = await getLocale();
+
+  // Providing all messages to the client
+  // side is the easiest way to get started
+  const messages = await getMessages();
   return (
     <Suspense>
-      <html lang="en">
+      <html lang={locale}>
         <body className={inter.className}>
-          <QueryClientProvider client={queryClient}>
-            <Provider>{children}</Provider>
-          </QueryClientProvider>
+          <QueryProvider>
+            <Provider>
+              <NextIntlClientProvider messages={messages}>
+                {children}
+              </NextIntlClientProvider>
+            </Provider>
+          </QueryProvider>
         </body>
       </html>
     </Suspense>

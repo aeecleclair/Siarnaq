9595ae06d06f4e282b7048eadbc360ae1074f093
Maxime Roucher <maximeroucher@gmail.com>
2024-07-04 14:54:26 +0200

    feat: adding callback dialog and page footer

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,136 +1,71 @@
 "use client";
 
+import { StatusDialog } from "@/components/custom/StatusDialog";
 import { AssociationPanel } from "@/components/user/AssociationPanel";
 import { CentralPanel } from "@/components/user/CentralPanel";
+import { useUser } from "@/hooks/useUser";
+import { useTokenStore } from "@/stores/token";
+import { useRouter, useSearchParams } from "next/navigation";
+import { useState } from "react";
 
 export default function Home() {
+  const { userId } = useTokenStore();
+  const { refetch } = useUser(userId);
+  const router = useRouter();
+  const searchParams = useSearchParams();
+  const code = searchParams.get("code");
+  const [isEndDialogOpened, setIsEndDialogOpened] = useState(true);
   return (
     <div className="flex min-h-screen w-full flex-col">
-      {/* <header className="sticky top-0 flex h-16 items-center gap-4 border-b bg-background px-4 md:px-6">
-        <nav className="hidden flex-col gap-6 text-lg font-medium md:flex md:flex-row md:items-center md:gap-5 md:text-sm lg:gap-6">
-          <Link
-            href="#"
-            className="flex items-center gap-2 text-lg font-semibold md:text-base"
-          >
-            <Package2 className="h-6 w-6" />
-            <span className="sr-only">Acme Inc</span>
-          </Link>
-          <Link
-            href="#"
-            className="text-muted-foreground transition-colors hover:text-foreground"
-          >
-            Dashboard
-          </Link>
-          <Link
-            href="#"
-            className="text-muted-foreground transition-colors hover:text-foreground"
-          >
-            Orders
-          </Link>
-          <Link
-            href="#"
-            className="text-muted-foreground transition-colors hover:text-foreground"
-          >
-            Products
-          </Link>
-          <Link
-            href="#"
-            className="text-muted-foreground transition-colors hover:text-foreground"
-          >
-            Customers
-          </Link>
-          <Link
-            href="#"
-            className="text-foreground transition-colors hover:text-foreground"
-          >
-            Settings
-          </Link>
-        </nav>
-        <Sheet>
-          <SheetTrigger asChild>
-            <Button
-              variant="outline"
-              size="icon"
-              className="shrink-0 md:hidden"
-            >
-              <Menu className="h-5 w-5" />
-              <span className="sr-only">Toggle navigation menu</span>
-            </Button>
-          </SheetTrigger>
-          <SheetContent side="left">
-            <nav className="grid gap-6 text-lg font-medium">
-              <Link
-                href="#"
-                className="flex items-center gap-2 text-lg font-semibold"
-              >
-                <Package2 className="h-6 w-6" />
-                <span className="sr-only">Acme Inc</span>
-              </Link>
-              <Link
-                href="#"
-                className="text-muted-foreground hover:text-foreground"
-              >
-                Dashboard
-              </Link>
-              <Link
-                href="#"
-                className="text-muted-foreground hover:text-foreground"
-              >
-                Orders
-              </Link>
-              <Link
-                href="#"
-                className="text-muted-foreground hover:text-foreground"
-              >
-                Products
-              </Link>
-              <Link
-                href="#"
-                className="text-muted-foreground hover:text-foreground"
-              >
-                Customers
-              </Link>
-              <Link href="#" className="hover:text-foreground">
-                Settings
-              </Link>
-            </nav>
-          </SheetContent>
-        </Sheet>
-        <div className="flex w-full items-center gap-4 md:ml-auto md:gap-2 lg:gap-4">
-          <form className="ml-auto flex-1 sm:flex-initial">
-            <div className="relative">
-              <Search className="absolute left-2.5 top-2.5 h-4 w-4 text-muted-foreground" />
-              <Input
-                type="search"
-                placeholder="Search products..."
-                className="pl-8 sm:w-[300px] md:w-[200px] lg:w-[300px]"
-              />
-            </div>
-          </form>
-          <DropdownMenu>
-            <DropdownMenuTrigger asChild>
-              <Button variant="secondary" size="icon" className="rounded-full">
-                <CircleUser className="h-5 w-5" />
-                <span className="sr-only">Toggle user menu</span>
-              </Button>
-            </DropdownMenuTrigger>
-            <DropdownMenuContent align="end">
-              <DropdownMenuLabel>My Account</DropdownMenuLabel>
-              <DropdownMenuSeparator />
-              <DropdownMenuItem>Settings</DropdownMenuItem>
-              <DropdownMenuItem>Support</DropdownMenuItem>
-              <DropdownMenuSeparator />
-              <DropdownMenuItem>Logout</DropdownMenuItem>
-            </DropdownMenuContent>
-          </DropdownMenu>
-        </div>
-      </header> */}
-      <main className="flex min-h-[calc(100vh_-_theme(spacing.16))] flex-1 flex-col gap-4 bg-muted/40 p-4 md:gap-8 md:p-10">
+      {code === "succeeded" && (
+        <StatusDialog
+          isOpened={isEndDialogOpened}
+          setIsOpened={setIsEndDialogOpened}
+          title="Paiement effectué"
+          description="Votre paiement a été effectué avec succès"
+          status="SUCCESS"
+          callback={() => {
+            refetch();
+            setIsEndDialogOpened(false);
+            router.replace("/?sellerId=recap");
+          }}
+        />
+      )}
+      {code === "refused" && (
+        <StatusDialog
+          isOpened={isEndDialogOpened}
+          setIsOpened={setIsEndDialogOpened}
+          title="Paiement refusé"
+          description="Votre paiement a été refusé. Vous pouvez réessayer de payer, si le problème persiste, veuillez nous contacter."
+          status="ERROR"
+          callback={() => {
+            setIsEndDialogOpened(false);
+            router.replace("/?sellerId=recap");
+          }}
+        />
+      )}
+      <main className="flex min-h-[calc(100vh_-_theme(spacing.32))] flex-1 flex-col gap-4 bg-muted/40 p-4 md:gap-8 md:p-10">
         <div className="mx-auto grid w-full max-w-6xl items-start gap-6 md:grid-cols-[180px_1fr] lg:grid-cols-[250px_1fr]">
           <AssociationPanel />
           <CentralPanel />
         </div>
       </main>
+      <footer className="py-6 md:px-8 md:py-0 border-t-2">
+        <div className="w-full max-w-screen-xl mx-auto p-4 md:py-8">
+          <div className="sm:flex sm:items-center sm:justify-between">
+            <p className="text-balance text-sm leading-loose text-muted-foreground">
+              Développé par Éclair
+            </p>
+            <span className="block text-sm text-gray-500 sm:text-center dark:text-gray-400">
+              © {new Date().getFullYear()}{" "}
+              <a href="" className="font-medium underline underline-offset-4">
+                Éclair
+              </a>
+              . Tout droits réservés.
+            </span>
+          </div>
+        </div>
+      </footer>
     </div>
   );
 }

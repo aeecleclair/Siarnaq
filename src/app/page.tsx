4ffcadab472132325356f4ebe546e6df1e0c05ef
Maxime Roucher <maximeroucher@gmail.com>
2024-08-14 11:38:10 +0200

    feat: capitalizing association name

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -55,12 +55,12 @@ export default function Home() {
         <div className="w-full max-w-screen-xl mx-auto p-4 md:py-8">
           <div className="sm:flex sm:items-center sm:justify-between">
             <p className="text-balance text-sm leading-loose text-muted-foreground">
-              Développé par Éclair
+              Développé par ÉCLAIR
             </p>
             <span className="block text-sm text-gray-500 sm:text-center dark:text-gray-400">
               © {new Date().getFullYear()}{" "}
               <a href="" className="font-medium underline underline-offset-4">
-                Éclair
+                ÉCLAIR
               </a>
               . Tout droits réservés.
             </span>

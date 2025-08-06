bb8edc55d03683eefdeb1396f35a23040edd6ea4
Marc-Andrieu <marc.andrieu@outlook.com>
2025-08-06 09:29:09 +0200

    Feat: complete the Typescript augmentation

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,10 @@
 import createNextIntlPlugin from "next-intl/plugin";
 
-const withNextIntl = createNextIntlPlugin();
+const withNextIntl = createNextIntlPlugin({
+  experimental: {
+    createMessagesDeclaration: "src/translations/fr.json",
+  },
+});
 
 /** @type {import('next').NextConfig} */
 const nextConfig = {

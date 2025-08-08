f64cf16877c745f7a8d1e5d03762c77c6aba913e
Marc-Andrieu <marc.andrieu@outlook.com>
2025-08-08 14:20:09 +0200

    rm unused

--- src/components/ui/use-toast.ts
+++ src/components/ui/use-toast.ts
@@ -7,8 +7,7 @@ import type {
   ToastActionElement,
   ToastProps,
 } from "@/components/ui/toast"
-import { Locale, useTranslations } from "next-intl"
-import { useParams } from "next/navigation"
+import { useTranslations } from "next-intl"
 
 const TOAST_LIMIT = 1
 const TOAST_REMOVE_DELAY = 1000000

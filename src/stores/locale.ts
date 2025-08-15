8bb872465865d52989f94a29f843a341a410ab0b
Marc-Andrieu <marc.andrieu@outlook.com>
2025-08-15 20:49:27 +0200

    Feat: new store for the locale setting

---
+++ src/stores/locale.ts
@@ -0,0 +1,22 @@
+import { Locale } from "next-intl";
+import { create } from "zustand";
+import { devtools, persist } from "zustand/middleware";
+
+interface LocaleStore {
+  localeStore: Locale;
+  setLocaleStore: (size: Locale) => void;
+}
+
+export const useLocaleStore = create<LocaleStore>()(
+  devtools(
+    persist(
+      (set) => ({
+        localeStore: "fr",
+        setLocaleStore: (localeStore) => set({ localeStore }),
+      }),
+      {
+        name: "locale-store",
+      },
+    ),
+  ),
+);

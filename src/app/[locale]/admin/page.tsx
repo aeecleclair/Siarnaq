65f9ab96579ebad2dd051fe47e87d083b3e23cea
Marc-Andrieu <marc.andrieu@outlook.com>
2025-08-17 23:37:31 +0200

    Fix: homogenous background color (muted)

--- src/app/[locale]/admin/page.tsx
+++ src/app/[locale]/admin/page.tsx
@@ -44,7 +44,7 @@ const AdminPage = () => {
   }, [status, setSize]);
 
   return (
-    <main className="grid flex-1 items-start gap-4 p-4 sm:px-6 sm:py-0 md:gap-8 mt-8">
+    <main className="grid flex-1 items-start gap-4 p-4 sm:px-6 sm:py-0 md:gap-8 pt-8 bg-muted/40">
       <Suspense fallback={<div>Loading...</div>}>
         {status && (
           <Card>
